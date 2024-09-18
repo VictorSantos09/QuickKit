ede70e8df3a311d38679be55a17f607c7c5501c6
Victor Santos <110687512+VictorSantos09@users.noreply.github.com>
2024-09-18 12:49:02 -0300

    upgrade BUILD_VERSION.bat

--- BUILD_VERSION.bat
+++ BUILD_VERSION.bat
@@ -1,42 +1,125 @@
 @echo off
 
-REM Executando o build da solução
+:inicio
+cls
+REM Solicitar caminho de destino
+call :getDestinationPath
+
+REM Solicitar quais DLLs copiar
+call :getUserChoices
+
+REM Executar build
+call :buildSolution
+
+REM Copiar DLLs com base nas escolhas do usuário
+call :copyDlls
+
+REM Compactar arquivos
+call :zipFiles
+
+REM Loop para regenerar, reiniciar ou encerrar
+call :menuOptions
+goto fim
+
+:getDestinationPath
+REM Pergunta o caminho de destino ao usuário
+set /p destPath=Informe o caminho onde deseja copiar as DLLs (ex: C:\destino): 
+
+REM Verifica se o usuário inseriu o caminho
+if "%destPath%"=="" (
+    echo Caminho nao informado. O script sera encerrado.
+    pause
+    exit /b
+)
+
+REM Criar a pasta de saída, se não existir
+if not exist "%destPath%" (
+    mkdir "%destPath%"
+)
+exit /b
+
+:getUserChoices
+REM Perguntar sobre cada DLL
+set /p copyQuickKit=Deseja copiar QuickKit.dll? (S/N): 
+set /p copyAspNetCore=Deseja copiar QuickKit.AspNetCore.dll? (S/N): 
+set /p copyCmd=Deseja copiar QuickKit.Cmd.dll? (S/N): 
+set /p copyResultTypes=Deseja copiar QuickKit.ResultTypes.dll? (S/N): 
+set /p copySecurity=Deseja copiar QuickKit.Security.dll? (S/N): 
+set /p copyShared=Deseja copiar QuickKit.Shared.dll? (S/N): 
+set /p copyBlazor=Deseja copiar QuickKit.Blazor.dll? (S/N): 
+exit /b
+
+:buildSolution
 echo Executando build da solução...
 dotnet build
+exit /b
 
-echo Criando output "dist"
-mkdir dist
+:copyDlls
+echo Copiando DLLs selecionadas...
 
-echo.
+REM Copiar as DLLs com base na escolha do usuário
+if /i "%copyQuickKit%"=="S" (
+    echo copiando QuickKit.dll
+    copy QuickKit\bin\debug\net7.0\QuickKit.dll "%destPath%"
+)
 
-echo copiando QuickKit.dll
-copy QuickKit\bin\debug\net7.0\QuickKit.dll ..\QuickKit\dist
+if /i "%copyAspNetCore%"=="S" (
+    echo copiando QuickKit.AspNetCore.dll
+    copy QuickKit.AspNetCore\bin\debug\net7.0\QuickKit.AspNetCore.dll "%destPath%"
+)
 
-echo copiando QuickKit.AspNetCore.dll
-copy QuickKit.AspNetCore\bin\debug\net7.0\QuickKit.AspNetCore.dll ..\QuickKit\dist
+if /i "%copyCmd%"=="S" (
+    echo copiando QuickKit.Cmd.dll
+    copy QuickKit.Cmd\bin\debug\net7.0\QuickKit.Cmd.dll "%destPath%"
+)
 
-echo copiando QuickKit.Cmd.dll
-copy QuickKit.Cmd\bin\debug\net7.0\QuickKit.Cmd.dll ..\QuickKit\dist
+if /i "%copyResultTypes%"=="S" (
+    echo copiando QuickKit.ResultTypes.dll
+    copy QuickKit.ResultTypes\bin\debug\net7.0\QuickKit.ResultTypes.dll "%destPath%"
+)
 
-echo copiando QuickKit.ResultTypes.dll
-copy QuickKit.ResultTypes\bin\debug\net7.0\QuickKit.ResultTypes.dll ..\QuickKit\dist
+if /i "%copySecurity%"=="S" (
+    echo copiando QuickKit.Security.dll
+    copy QuickKit.Security\bin\debug\net7.0\QuickKit.Security.dll "%destPath%"
+)
 
-echo copiando QuickKit.Security.dll
-copy QuickKit.Security\bin\debug\net7.0\QuickKit.Security.dll ..\QuickKit\dist
+if /i "%copyShared%"=="S" (
+    echo copiando QuickKit.Shared.dll
+    copy QuickKit.Shared\bin\debug\net7.0\QuickKit.Shared.dll "%destPath%"
+)
 
-echo copiando QuickKit.Shared
-copy QuickKit.Shared\bin\debug\net7.0\QuickKit.Shared.dll ..\QuickKit\dist
+if /i "%copyBlazor%"=="S" (
+    echo copiando QuickKit.Blazor.dll
+    copy QuickKit.Blazor\bin\debug\net8.0\QuickKit.Blazor.dll "%destPath%"
+)
+exit /b
 
-echo copiando QuickKit.Blazor
-copy QuickKit.Blazor\bin\debug\net8.0\QuickKit.Blazor.dll ..\QuickKit\dist
-
-if exist "dist\dist.zip" del "dist\dist.zip"
-
-REM Compacta os arquivos na pasta "dist" usando 7-Zip
+:zipFiles
 echo Compactando arquivos...
-"C:\Program Files\7-Zip\7z.exe" a -r dist\dist.zip dist\*
+if exist "%destPath%\dist.zip" del "%destPath%\dist.zip"
+"C:\Program Files\7-Zip\7z.exe" a -r "%destPath%\dist.zip" "%destPath%\*"
+exit /b
 
+:menuOptions
+echo Escolha uma opcao:
+echo 1. Regenerar no mesmo caminho
+echo 2. Informar novo caminho
+echo 3. Encerrar
+set /p option=Digite o numero da opcao e pressione Enter: 
 
-echo Finalizado
+if "%option%"=="1" (
+    call :copyDlls
+    call :zipFiles
+    goto menuOptions
+)
 
-pause done
+if "%option%"=="2" goto inicio
+if "%option%"=="3" goto fim
+
+echo Opcao invalida, tente novamente.
+goto menuOptions
+
+:fim
+echo Script finalizado.
+pause
+exit
